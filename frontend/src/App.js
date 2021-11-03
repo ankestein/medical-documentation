854ca6c9244f3ac8c16c467f16aee7a195e78c0f
Anke <a.stein@posteo.de>
2021-11-03 11:08:39 +0100

    Remove logs and greeting.json test data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,15 @@
 import {getGreetings} from './service/greetingApiService';
 import {useEffect, useState} from 'react';
-import jsonGreetings from './greeting.json';
 
 export default function App() {
 	const [greetings, setGreetings] = useState([]);
 
 	useEffect(() => {
-		getGreetings()
-			//.then((greetings) => setGreetings(greetings))
-			.then((greetings) => {
-				setGreetings(greetings);
-				console.log('greetings:');
-				console.log(greetings);
-			});
+		getGreetings().then((greetings) => setGreetings(greetings));
 	}, []);
 
 	return (
 		<>
-			<h1>JSON Greetings</h1>
-			<ul>
-				{jsonGreetings.map((jsonGreeting) => {
-					return <li key={jsonGreeting.id}>{jsonGreeting.greetingText}</li>;
-				})}
-			</ul>
-
-			<h1>Database Greetings</h1>
 			<ul>
 				{greetings.map((greeting) => {
 					return <li key={greeting.id}>{greeting.greetingText}</li>;
--- frontend/src/service/greetingApiService.js
+++ frontend/src/service/greetingApiService.js
@@ -1,15 +1,8 @@
 import axios from 'axios';
 
 export function getGreetings() {
-	return (
-		axios
-			.get('/api/greeting')
-			//.then((response) => response.data)
-			.then((response) => {
-				console.log('GET response data:');
-				console.log(response.data);
-				return response.data;
-			})
-			.catch(console.error)
-	);
+	return axios
+		.get('/api/greeting')
+		.then((response) => response.data)
+		.catch(console.error);
 }
