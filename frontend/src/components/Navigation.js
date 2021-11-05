8c5c36beee2b95c5fa056571e611308ff05e13f4
Anke <a.stein@posteo.de>
2021-11-05 12:22:03 +0100

    Add navigation bar
    Install React Router and Material UI

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,38 @@
+import {BottomNavigation, BottomNavigationAction} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
+export default function Navigation() {
+	const [value, setValue] = useState('');
+	const history = useHistory();
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+		history.push(newValue);
+	};
+
+	return (
+		<BottomNavigation showLabels value={value} onChange={handleChange}>
+			<BottomNavigationAction label='Home' value='/' icon={<HomeIcon />} />
+			<BottomNavigationAction
+				label='Symptoms'
+				value='/symptoms'
+				icon={<ThermostatIcon />}
+			/>
+			<BottomNavigationAction
+				label='Doctors'
+				value='/doctors'
+				icon={<PersonIcon />}
+			/>
+			<BottomNavigationAction
+				label='Appointments'
+				value='/appointments'
+				icon={<EventNoteIcon />}
+			/>
+		</BottomNavigation>
+	);
+}
