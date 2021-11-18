f2378d3e294fbcfd5cbad481fa0592ee34cc56cd
Anke <a.stein@posteo.de>
2021-11-18 21:28:50 +0100

    Fix sending data to backend: modify radio buttons and Autocomplete
    Add radio buttons for testType; Autocomplete not working yet and code outcommented

--- frontend/src/pages/NewCovidTest.js
+++ frontend/src/pages/NewCovidTest.js
@@ -11,16 +11,29 @@ import {
 	RadioGroup,
 } from '@material-ui/core';
 import DateAdapter from '@mui/lab/AdapterMoment';
-import {Autocomplete, DateTimePicker, LocalizationProvider} from '@mui/lab';
+import {DateTimePicker, LocalizationProvider} from '@mui/lab';
 
 export default function NewCovidTest({setAllCovidTests}) {
+	{
+		/*}
 	const initialCovidTest = {
 		testType: '',
 		dateTime: null,
 		result: '',
 	};
+	*/
+	}
+	{
+		/*}
+	const initialCovidTest = {
+		testType: null,
+		dateTime: null,
+		result: '',
+	};
+*/
+	}
 
-	const [newCovidTest, setNewCovidTest] = useState(initialCovidTest);
+	const [newCovidTest, setNewCovidTest] = useState({});
 	const history = useHistory();
 
 	const handleChange = (event) => {
@@ -37,35 +50,106 @@ export default function NewCovidTest({setAllCovidTests}) {
 			return;
 		}
 		submitCovidTest(newCovidTest).catch(console.error);
-		setNewCovidTest(initialCovidTest);
+		//setNewCovidTest(initialCovidTest);
+		setNewCovidTest({});
 		getCovidTests()
 			.then((covidTests) => setAllCovidTests(covidTests))
 			.catch(console.error)
 			.then(() => history.push('/covid-tests'));
 	};
 
+	{
+		/*}
 	const testTypes = [
-		'Antigen test - lollipop',
-		'Antigen test - nasal',
-		'Antigen test - nasopharyngeal',
-		'Antigen test - saliva',
-		'PCR test',
+		{value: 'ANTIGEN_LOLLIPOP', label: 'Antigen test - lollipop'},
+		{value: 'ANTIGEN_NASAL', label: 'Antigen test - nasal'},
+		{value: 'ANTIGEN_NASOPHARYNGEAL', label: 'Antigen test - nasopharyngeal'},
+		{value: 'ANTIGEN_SALIVA', label: 'Antigen test - saliva'},
+		{value: 'PCR_TEST', label: 'PCR test'},
 	];
+  */
+	}
+	{
+		/*}
+const testTypes = [
+  'ANTIGEN_LOLLIPOP',
+  'ANTIGEN_NASAL',
+  'ANTIGEN_NASOPHARYNGEAL',
+  'ANTIGEN_SALIVA',
+  'PCR_TEST',
+];
+	*/
+	}
 
 	return (
 		<PageLayout>
 			<Form onSubmit={handleSubmit}>
+				{/*}
+      <Autocomplete
+        id='select-test-type'
+        options={testTypes}
+        isOptionEqualToValue={(option, value) => option.value === value.value}
+        value={newCovidTest.testType}
+        required={true}
+        name='testType'
+        onChange={handleChange}
+        renderInput={(params) => (
+          <TextField {...params} label='Type of COVID test' />
+        )}
+      />
+      */}
+
+				{/*}
 				<Autocomplete
-					id='select-test-type'
 					options={testTypes}
+					isOptionEqualToValue={(option, value) => option.value === value.value}
 					value={newCovidTest.testType}
-					required={true}
 					name='testType'
+					getOptionLabel={(option) => option.value}
+					renderOption={(option) => <>{option.label}</>}
 					onChange={handleChange}
 					renderInput={(params) => (
 						<TextField {...params} label='Type of COVID test' />
 					)}
 				/>
+     */}
+
+				<FormControl component='fieldset'>
+					<FormLabel component='legend'>Type of COVID test</FormLabel>
+					<RadioGroup
+						row
+						aria-label='test-type'
+						name='testType'
+						value={newCovidTest.testType}
+						onChange={handleChange}
+					>
+						<FormControlLabel
+							value='ANTIGEN_LOLLIPOP'
+							control={<Radio />}
+							label='Antigen test - lollipop'
+						/>
+						<FormControlLabel
+							value='ANTIGEN_NASAL'
+							control={<Radio />}
+							label='Antigen test - nasal'
+						/>
+						<FormControlLabel
+							value='ANTIGEN_NASOPHARYNGEAL'
+							control={<Radio />}
+							label='Antigen test - nasopharyngeal'
+						/>
+						<FormControlLabel
+							value='ANTIGEN_SALIVA'
+							control={<Radio />}
+							label='Antigen test - saliva'
+						/>
+						<FormControlLabel
+							value='PCR_TEST'
+							control={<Radio />}
+							label='PCR test'
+						/>
+					</RadioGroup>
+				</FormControl>
 
 				<LocalizationProvider dateAdapter={DateAdapter}>
 					<DateTimePicker
@@ -80,14 +164,20 @@ export default function NewCovidTest({setAllCovidTests}) {
 
 				<FormControl component='fieldset'>
 					<FormLabel component='legend'>Result</FormLabel>
-					<RadioGroup row aria-label='result' name='result-radio-buttons-group'>
+					<RadioGroup
+						row
+						aria-label='result'
+						name='result'
+						value={newCovidTest.result}
+						onChange={handleChange}
+					>
 						<FormControlLabel
-							value='negative'
+							value='NEGATIVE'
 							control={<Radio />}
 							label='negative'
 						/>
 						<FormControlLabel
-							value='positive'
+							value='POSITIVE'
 							control={<Radio />}
 							label='positive'
 						/>
