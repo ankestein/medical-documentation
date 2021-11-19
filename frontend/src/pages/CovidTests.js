6b9d62e1f326fb5ddbb6554ac78fdc57b8a23703
Anke <a.stein@posteo.de>
2021-11-19 15:09:09 +0100

    Add FAB to navigate to /new-covid-test

--- frontend/src/pages/CovidTests.js
+++ frontend/src/pages/CovidTests.js
@@ -1,3 +1,30 @@
+import {Fab} from '@material-ui/core';
+import AddIcon from '@mui/icons-material/Add';
+import styled from 'styled-components/macro';
+import {Link} from 'react-router-dom';
+
 export default function CovidTests() {
-	return null;
+	return (
+		<PageLayout>
+			<StyledH1>COVID19 Tests</StyledH1>
+
+			<Fab
+				color='primary'
+				size='small'
+				aria-label='add-covid-test'
+				component={Link}
+				to='/new-covid-test'
+			>
+				<AddIcon />
+			</Fab>
+		</PageLayout>
+	);
 }
+
+const PageLayout = styled.div`
+	margin-bottom: 60px;
+`;
+
+const StyledH1 = styled.h1`
+	margin-left: 12px;
+`;
