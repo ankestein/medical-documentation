766b0553ca0e2d3252aca4124e78f855cff17baa
Anke <a.stein@posteo.de>
2021-11-23 17:13:24 +0100

    Add Spring Security with Security Config, AppUser in MongoDB and hashed password

---
+++ backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package de.neuefische.backend.security.config;
+
+import de.neuefische.backend.security.service.AppUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserDetailService appUserDetailService;
+
+    @Autowired
+    public SecurityConfig(AppUserDetailService appUserDetailService) {
+        this.appUserDetailService = appUserDetailService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
