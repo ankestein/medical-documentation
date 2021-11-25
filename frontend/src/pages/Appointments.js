adf2b0df3269e634e104e1fa96cc1570d844ef50
Anke <a.stein@posteo.de>
2021-11-25 16:22:14 +0100

    Recreate FAB with styled components

--- frontend/src/pages/Appointments.js
+++ frontend/src/pages/Appointments.js
@@ -27,6 +27,10 @@ export default function Appointments({allDoctors}) {
 				onClick={handleClickAddAppointment}
 			/>
 
+			<Circle>
+				<AddIcon />
+			</Circle>
+
 			<CardContainer>
 				{allDoctors.map((doctor) => {
 					return doctor.appointments.map((appointment) => {
@@ -56,4 +60,17 @@ const StyledH1 = styled.h1`
 	font-weight: 500;
 	font-size: 18px;
 	margin: 12px;
+`;
+
+const Circle = styled.div`
+	cursor: pointer;
+	border-radius: 50%;
+	width: 40px;
+	height: 40px;
+	background: var(--primary-background);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2),
+		0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 `;
