7d050a8e3fe2302cd1ef4ba0514a312e18358e2b
Anke <a.stein@posteo.de>
2021-11-25 15:50:14 +0100

    Add logo to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,9 @@
-import {AppBar, IconButton, Toolbar, Typography} from '@mui/material';
+import {AppBar, IconButton, Toolbar} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import {Login} from '@mui/icons-material';
 import {Box} from '@material-ui/core';
 import {useHistory} from 'react-router-dom';
+import logo from '../styling/MedDoc-logo-transparent.png';
 
 export default function Header() {
 	const history = useHistory();
@@ -12,26 +13,23 @@ export default function Header() {
 	};
 
 	return (
-		<Box sx={{flexGrow: 1}}>
+		<Box>
 			<AppBar position='sticky'>
 				<Toolbar>
 					<IconButton
 						edge='start'
-						color='inherit'
 						aria-label='menu'
-						sx={{mr: 2}}
+						sx={{marginRight: 2, color: 'white'}}
 					>
 						<MenuIcon />
 					</IconButton>
-					<Typography
-						variant='h6'
-						color='inherit'
-						component='div'
-						sx={{flexGrow: 1}}
+
+					<img src={logo} width='130' alt='MedDoc' />
+
+					<IconButton
+						onClick={handleClick}
+						sx={{marginLeft: 17, color: 'white'}}
 					>
-						MedDoc
-					</Typography>
-					<IconButton color='inherit' onClick={handleClick}>
 						<Login />
 					</IconButton>
 				</Toolbar>
